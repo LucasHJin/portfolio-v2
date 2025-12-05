b4c4b8eb34797fd4afac70387d32cb4b79dd54f7
Lucas Jin <kblazer20@gmail.com>
2025-12-05 11:36:55 -0500

    feat: complete navbar

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 export default function Portfolio() {
   return (
     <div>
-      <Navbar />
+      <Navbar currPage="portfolio"/>
       PROJECTS
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,28 @@
-export default function Navbar() {
+import Link from "next/link";
+
+export default function Navbar({
+  currPage,
+}: {
+  currPage: "home" | "portfolio";
+}) {
   return (
-    <div>NAVBAR</div>
+    <nav className="flex justify-between mb-3">
+      <Link
+        href="/"
+        className={`font-garamond text-5xl
+        relative after:absolute after:-bottom-1.5 after:left-0 after:h-0.5 after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:after:w-full
+        ${currPage === "home" ? "text-foreground" : "text-highlight"}`}
+      >
+        Lucas Jin
+      </Link>
+      <Link
+        href="/portfolio"
+        className={`font-garamond text-5xl 
+        relative after:absolute after:-bottom-1.5 after:left-0 after:h-0.5 after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:after:w-full
+        ${currPage === "portfolio" ? "text-foreground" : "text-highlight"}`}
+      >
+        Portfolio
+      </Link>
+    </nav>
   );
 }
